6f5fbcd5e8398fe66d141dc1737aec503c969fc2
Jon Pither <jon.pither@gmail.com>
2017-01-09 15:59:51 +0000

    riemann dash

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Packer-Riemann
 
-Package up Riemann into an AMI
+Package up Riemann and Riemann-dash into an AMI.
 
 ```
 packer build -var 'riemann_version=0.2.12' riemann.json
--- install-riemann.sh
+++ install-riemann.sh
@@ -7,6 +7,10 @@ apt-get update
 apt-get -y install default-jre
 apt-get -y install default-jdk
 
+# Riemann Dash (there if you want it)
+apt-get -y install ruby-full
+gem install riemann-dash
+
 mkdir /riemann
 cd /riemann
 curl -skL https://github.com/riemann/riemann/releases/download/$RIEMANN_VERSION/riemann-$RIEMANN_VERSION.tar.bz2 | bunzip2 | tar -x
