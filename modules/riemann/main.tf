85e05e3006a6e0bc88af088a46746dc37840bad9
Jon Pither <jon.pither@gmail.com>
2017-01-12 16:40:02 +0000

    Riemann redeploy

--- modules/riemann/main.tf
+++ modules/riemann/main.tf
@@ -25,27 +25,6 @@ resource "aws_security_group" "inbound" {
   tags {
     Name = "${var.system_name}_riemann"
   }
-}
-
-resource "aws_instance" "riemann" {
-  ami                    = "${var.ami_image_id}"
-  instance_type          = "${var.instance_type}"
-  availability_zone      = "${var.availability_zones[0]}"
-  vpc_security_group_ids = ["${aws_security_group.inbound.id}"]
-  key_name               = "${var.key_name}"
-  private_ip             = "${var.private_ip}"
-
-  tags {
-    Name = "${var.system_name}-riemann"
-    Type = "Riemann"
-  }
-
-  connection {
-    user        = "ubuntu"
-    private_key = "${file("${var.key_path}")}"
-  }
-
-  provisioner "remote-exec" {
     inline = [
       "sudo touch /etc/riemann.config",
       "sudo chown -R ubuntu:ubuntu /etc/riemann.config",
